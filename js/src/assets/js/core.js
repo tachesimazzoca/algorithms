ee2c519b61e8836a3eedcfe784e6920bd06a147a
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-06-15 15:20:25 +0900

    Add Extendable for the prototype chain

--- js/src/assets/js/core.js
+++ js/src/assets/js/core.js
@@ -1,12 +1,83 @@
+/**
+ * @module algorithms-js
+ */
 (function() {
   var root = this;
   var previousAlgsJS = root.algsJS;
   var algsJS = root.algsJS = {};
 
+  // VERSION
   algsJS.VERSION = "0.0.0";
 
+  // noConflict
   algsJS.noConflict = function() {
     root.algsJS = previousAlgsJS;
     return this;
+  };
+
+  /**
+   * Provides static utility methods.
+   *
+   * @class Util
+   */
+  var Util = algsJS.Util = {};
+
+  /**
+   * Extends a given object with all the properties
+   *
+   * @static
+   * @method extend
+   * @param {Object} An object that will receive the new properties
+   * @param {Object} Additional objects containing properties to merge in
+   */
+  Util.extend = function(obj) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0; i < args.length; i++) {
+      if (args[i]) {
+        for (var prop in args[i]) {
+          obj[prop] = args[i][prop];
+        }
+      }
+    }
+    return obj;
+  };
+
+  /**
+   * Functions to set up the prototype chain.
+   *
+<pre><code>var Animal = function(name) {
+  this.name = name;
+};
+algsJS.Util.extend(Animal, algsJS.Extendable);
+var Bird = Animal.extend({
+  fly: function() { return "Fly! " + this.name; }
+}):
+</code></pre>
+   *
+   * @class Extendable
+   */
+  algsJS.Extendable = {
+    extend: function(protoProps, staticProps) {
+      var parent = this;
+      var child;
+
+      if (protoProps && protoProps.hasOwnProperty('constructor')) {
+        child = protoProps.constructor;
+      } else {
+        child = function() { return parent.apply(this, arguments); };
+      }
+
+      Util.extend(child, parent, staticProps);
+
+      var Surrogate = function() { this.constructor = child; };
+      Surrogate.prototype = parent.prototype;
+      child.prototype = new Surrogate();
+
+      if (protoProps) Util.extend(child.prototype, protoProps);
+
+      child.__super__ = parent.prototype;
+
+      return child;
+    }
   };
 }).call(this);
--- js/src/tests/unit/core.js
+++ js/src/tests/unit/core.js
@@ -4,4 +4,45 @@
   test("VERSION", function() {
     deepEqual(algsJS.VERSION, "0.0.0");
   });
+
+  test("Util.extend", function() {
+    var Util = algsJS.Util;
+    var obj = Util.extend({}, { foo: "bar", baz: 123 });
+    deepEqual(obj, { foo: "bar", baz: 123 });
+  });
+
+  test("Extendable", function() {
+    var Util = algsJS.Util;
+
+    var ClassA = function(name) {
+      this.name = name;
+    };
+    ClassA.prototype = {
+      getName: function() {
+        return this.name;
+      }
+
+    , setName: function(name) {
+        this.name = name;
+        return this;
+      }
+    };
+    var a = new ClassA("foo");
+    deepEqual(a.getName(), "foo");
+    deepEqual(a.setName("bar").getName(), "bar");
+
+    Util.extend(ClassA, algsJS.Extendable);
+    var ClassB = ClassA.extend({
+      constructor: function(name, age) {
+        this.age = age;
+        ClassA.apply(this, [name]);
+      }
+
+    , sayHello: function() {
+        return "Hello, call me " + this.name + ". I'm " + this.age + " years old.";
+      }
+    });
+    var b = new ClassB("baz", 25);
+    deepEqual(b.sayHello(), "Hello, call me baz. I'm 25 years old.");
+  });
 })();
