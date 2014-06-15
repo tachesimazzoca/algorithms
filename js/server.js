74bd84c93c4018cb407a0d5eb2992e15a77f00a9
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-06-15 15:18:10 +0900

    Set a document directory to argv[2]

--- js/server.js
+++ js/server.js
@@ -3,8 +3,11 @@ var host = "0.0.0.0";
 var port = 4000;
 var express = require("express");
 
+var args = process.argv.slice(2);
+var docroot = (args.length > 0) ? args[0] : __dirname + '/build';
+
 var app = express();
 app.use(app.router);
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(docroot));
 
 app.listen(port, host);
