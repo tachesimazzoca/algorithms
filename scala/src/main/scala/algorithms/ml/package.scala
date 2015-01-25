95920f0f7d98010aa53e2eff3a17d118be8da167
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-01-25 23:27:10 +0900

    Add gradient descent function

--- scala/src/main/scala/algorithms/ml/package.scala
+++ scala/src/main/scala/algorithms/ml/package.scala
@@ -7,4 +7,31 @@ package object ml {
     }
     se.sum / data.size
   }
+
+  def gradientDecent(data: Seq[(Double, Double)],
+                     max: Int,
+                     position: (Double, Double),
+                     rate: Double): (Double, Double, Int) = {
+    val N = data.size
+
+    def loop(acc: (Double, Double, Int)): (Double, Double, Int) = {
+      val a = acc._1
+      val b = acc._2
+      val c = acc._3
+      if (c == 0)
+        throw new Error("Any local optima could not be found. " + acc)
+      val ga = (data.map { case (x, y) => (a + b * x) - y }).sum / N
+      val gb = (data.map { case (x, y) => ((a + b * x) - y) * x }).sum / N
+      val a2 = a - rate * ga
+      val b2 = b - rate * gb
+      if (isGoodEnough(a, a2) && isGoodEnough(b, b2)) (a2, b2, max - c)
+      else loop(a2, b2, c - 1)
+    }
+
+    def isGoodEnough(a: Double, b: Double): Boolean = {
+      math.abs(a - b) < 0.0001d
+    }
+
+    loop((position._1, position._2, max))
+  }
 }
--- scala/src/test/scala/algorithms/ml/LinearRegressionSuite.scala
+++ scala/src/test/scala/algorithms/ml/LinearRegressionSuite.scala
@@ -5,8 +5,8 @@ import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class MSESuite extends FunSuite {
-  test("mse h(x) = a + b * x") {
+class LinearRegressionSuite extends FunSuite {
+  test("mse") {
     // h(x) = a + b * x, if a = 0, b = 2
     // (0^2 + 0^2 + 0^2) / 3 = 0
     assert(0 === mse(Seq((1d, 2d), (2d, 4d), (3d, 6d)), x => 2 * x))
@@ -14,5 +14,13 @@ class MSESuite extends FunSuite {
     // h(x) = a + b * x, if a = 0, b = 0.5
     // (0^2 + 0^2 + 0^2 + 0^2) / 4 = 0
     assert(0 === mse(Seq((1d, 0.5d), (2d, 1d), (4d, 2d), (0d, 0d)), x => 0.5 * x))
+  }
+
+  test("gradientDescent") {
+    val data = Seq((1d, 2d), (2d, 4d), (3d, 6d))
+    intercept[Error] {
+      gradientDecent(data, 412, (1d, 1d), 0.1d)
+    }
+    gradientDecent(data, 413, (1d, 1d), 0.1d)
   }
 }
